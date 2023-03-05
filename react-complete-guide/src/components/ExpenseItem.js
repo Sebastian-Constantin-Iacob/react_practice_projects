7717d052fa05672f32b6007d4d20b8f868dc9573
Constantin-Sebastian <Iacob1832@gmail.com>
2023-03-05 06:22:55 +0000

    Parent->Child(1st)->Child(2nd)

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,18 +1,12 @@
 import "./ExpenseItem.css";
+import "./ExpenseDate";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
 
-    const day = props.date.toLocaleString('en-US', { month: 'long'});
-    const month = props.date.toLocaleString('en-US', { day: '2-digit'});
-    const year = props.date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div>
         <h2 className="expense-item__description">{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
